4bdf98ba9eb5cbe1088903af2315f4b274fc2ee0
Tihana Krajtmajer <tihanakrajtmajer@gmail.com>
2023-08-19 16:32:03 +0200

    Add sorting by option

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="search-bar" class="el-tertiary">
-    <button class="button-icon align" @click="searchWithKeyword"><img src="../assets/ic_search@3x.png" class="search-icon"></button>
+    <button class="button-plain align" @click="searchWithKeyword"><img src="../assets/ic_search@3x.png" class="search-icon"></button>
     <input id="input-field" class="font-input-field align el-tertiary" type="search" placeholder="Search for a house" @keydown.enter.prevent="handleInput($event.target.value)">
-    <button class="button-icon align" @click="clearInput"><img src="../assets/ic_clear@3x.png" class="search-icon"></button>
+    <button class="button-plain align" @click="clearInput"><img src="../assets/ic_clear@3x.png" class="search-icon"></button>
   </div>
 </template>
 
@@ -43,13 +43,6 @@ export default {
 #input-field {
   border: none;
   padding: 10px;
-}
-.button-icon {
-  margin: 0;
-  padding: 0;
-  border: none;
-  background: none;
-  cursor: pointer;
 }
 .search-icon {
   width: 20px;
---
+++ src/components/SortOptions.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="sort-options">
+    <button @click="sortBy('price')">Price</button>
+    <button @click="sortBy('size')">Size</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SortOptions',
+  data() {
+    return {
+      currentOption: '',
+      currentOrder: 1
+    };
+  },
+  methods: {
+    sortBy(option) {
+      if(this.currentOption === option) {
+        this.currentOrder *= -1;
+      }
+      else {
+        this.currentOrder = 1;
+        this.currentOption = option;
+      }
+      console.log('iz options ', this.currentOption, this.currentOrder);
+      this.$emit('sortByOption', {option: this.currentOption, order: this.currentOrder});
+    }
+  }
+}
+</script>
