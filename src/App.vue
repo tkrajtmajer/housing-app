5d48fdd0595f1a9e89203786bdd080d740896848
Tihana Krajtmajer <tihanakrajtmajer@gmail.com>
2023-08-23 17:29:17 +0200

    Restructure app.vue to include more pages

--- src/App.vue
+++ src/App.vue
@@ -1,127 +1,26 @@
 <template>
-  <NavBar/>
-  <div id="main-content">
-    <h1>Houses</h1>
-    <br><br>
-    <div class="vertical-section">
-      <SearchBar @newSearch="handleSearch" @updateResultsCount="updateCounter"/>
-      <SortOptions @sortByOption="sortByOption"/>
-    </div>
-    <div v-if="updateNrResults && !noResults">
-      <br><br>
-      <h2><span id="nrResults">{{ queriedData.length }}</span> results found</h2>
-    </div>
-    <div id="listings">
-      <HouseListing :responseData="queriedData"/>
-    </div>
-    <div v-if="noResults">
-      <EmptySearch/>
-    </div>
-  </div>
+  <header>
+    <NavBar/>
+  </header>
+  <main id="main-content">
+    <router-view></router-view>
+  </main>
 </template>
 
 <script>
-import NavBar from './components/NavBar.vue'
-import HouseListing from './components/HouseListing.vue'
-import SearchBar from './components/SearchBar.vue'
-import EmptySearch from './components/EmptySearch.vue'
-import SortOptions from './components/SortOptions.vue'
-import House from './models/House.js'
+import NavBar from './components/elements/NavBar.vue'
 
 export default {
   name: 'App',
   components: {
-    NavBar,
-    HouseListing,
-    SearchBar,
-    EmptySearch,
-    SortOptions
-  },
-  data() {
-    return {
-      responseData: [],
-      queriedData: [],
-      updateNrResults: false
-    };
-  },
-  methods: {
-    async getHouseListings() {
-      const apikey = 'qMjVn2QEhdDCeO95Lk1wHcI7FU3gJomG'; 
-
-      try {
-        const response = await fetch('https://api.intern.d-tt.nl/api/houses', {
-          headers: {
-            'X-Api-Key': apikey
-          }
-        });
-
-        if (response.ok) {
-          const data = await response.json();
-
-          for(const el of data) {
-            // process the data into separate house objects
-            const house = new House(el.image, el.location.street, el.price, el.location.zip, el.location.city, el.rooms.bedrooms, el.rooms.bathrooms, el.size);
-            this.responseData.push(house);
-          }
-        } else {
-          console.error(response.statusText);
-        }
-      }
-      catch (e) {
-        // Handle error
-        console.log(e);
-      }
-    },
-    handleSearch(query) {
-      this.queriedData = [];
-      
-      if(query === '') {
-        this.queriedData = this.responseData;
-      }
-      else if(query === undefined) {
-        throw new Error( "undefined query");
-      }
-      else {
-        this.queriedData = this.responseData.filter(house => {
-          return (
-            house.address.toLowerCase().includes(query.toLowerCase()) ||
-            house.price.toString().includes(query.toLowerCase()) || 
-            house.postcode.toLowerCase().includes(query.toLowerCase()) || 
-            house.city.toLowerCase().includes(query.toLowerCase()) ||
-            house.size.toString().includes(query.toLowerCase()) 
-          );
-        });
-      }
-
-      if (this.queriedData.length === 0) {
-        this.noResults = true;
-      } else {
-        this.noResults = false;
-      }
-    },
-    updateCounter(flag) {
-      this.updateNrResults = flag;
-    },
-    sortByOption({option, order}) {
-      console.log('iz maina ', option, order);
-      if(option === 'price') {
-        this.queriedData.sort((a, b) => (a.price - b.price) * order);
-      }
-      else {
-        this.queriedData.sort((a, b) => (a.size - b.size) * order);
-      }
-    }
-  },
-  created() {
-    this.getHouseListings();
-    this.queriedData = this.responseData;
+    NavBar
   }
 }
 </script>
 
 <style>
 #main-content {
-  margin: 150px 250px 50px 250px;
+  margin: 150px 330px 50px 330px;
 }
 #listings {
   margin-top: 30px;
