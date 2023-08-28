676a99391dea5f8b824a0412ddbf14a077d1a9c8
Tihana Krajtmajer <tihanakrajtmajer@gmail.com>
2023-08-28 03:24:44 +0200

    Add components for deletion

---
+++ src/components/elements/DeleteAlert.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="delete-alert" class="el-bg-one">
+    <div id="alert-content" class="el-bg-two">
+      <h2>Delete listing</h2><br>
+      <p class="font-body-text">Are you sure you want to delete this listing?<br>
+      This action cannot be undone.</p><br><br>
+      <button @click="this.$emit('confirmDeletion')" class="button-basic el-primary font-buttons">
+        YES, DELETE
+      </button><br><br>
+      <button @click="this.$emit('cancelDeletion')" class="button-basic el-secondary font-buttons">
+        GO BACK
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#delete-alert {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  top: 0;
+  left: 0;
+  z-index: 2;
+  background-color: rgba(0.2, 0.2, 0.2, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center; 
+}
+#alert-content {
+  width: 600px;
+  padding: 50px;
+  border-radius: 10px;
+}
+.button-basic {
+  width: 75%;
+  border-radius: 10px;
+  padding: 15px 20px 15px 20px;
+}
+</style>
---
+++ src/components/elements/DeleteButton.vue
@@ -0,0 +1,48 @@
+<template>
+  <button @click="showAlert" class="button-basic">
+    <img src="../../assets/ic_delete@3x.png" class="icon">
+  </button>
+  <div v-if="this.alertIsVisible === true">
+    <DeleteAlert @confirmDeletion="deleteHouse" @cancelDeletion="hideAlert" />
+  </div>
+</template>
+
+<script>
+import DeleteAlert from "./DeleteAlert.vue";
+
+export default {
+  components: {
+    DeleteAlert
+  },
+  data() {
+    return {
+      alertIsVisible: false
+    }
+  },    
+  methods: {
+    showAlert() {
+      this.alertIsVisible = true;
+      document.body.style.overflow = 'hidden';
+    },
+    hideAlert() {
+      this.alertIsVisible = false;
+      document.body.style.overflow = 'auto';
+    },
+    deleteHouse() {
+      this.hideAlert();
+      this.$emit('deleteHouse');
+    }
+  }
+}
+</script>
+
+<style scoped>
+.button-basic {
+  background: none;
+  float: right;
+}
+.icon {
+  max-height: 25px;
+  margin-right: 0;
+}
+</style>
